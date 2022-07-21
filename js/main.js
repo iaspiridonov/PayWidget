263d4fc3d29f943519ecb54f472ae9875ae5c49c
Ilya S <spiridonov.ilya81@gmail.com>
2022-07-22 00:54:54 +0300

    Add cookies

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    
     $bankImage = $('.js-bank-name-img');
     
     $('.card--select').click(function (e) { 
@@ -19,11 +18,13 @@ $(document).ready(function () {
             $(this).parents('form').find('input[name=bank]').val(bankName);
             $(this).parents('form').find('#selected-bank').text(bankName);
             
-            // Save bank name to session
-            sessionStorage['bankName']  = bankName;
-            sessionStorage['bankImage'] = bankImage;
+            // Save bank name to cookie
             
-            $bankImage.attr('src', sessionStorage['bankImage']);
+            // Пример использования:
+            setCookie('bankName', bankName);
+            setCookie('bankImage', bankImage);
+            
+            $bankImage.attr('src', getCookie('bankImage'));
             if ($bankImage.attr('src')) {
                 $bankImage.show();
             }
@@ -31,9 +32,13 @@ $(document).ready(function () {
             // Show next block
             $('.js-after-select-bank').show('fast');
             
-            $('#js-display-two-bank-name').text('"'+ sessionStorage['bankName'] + '".');
+            $('#js-display-two-bank-name').text('"'+ getCookie('bankName') + '".');
         }
         
+    });
+    
+    $('.js-select-pay-amount').find('input').change(function (e) { 
+        $('#js-rate-filed').val($(this).val());
     });
     
     $('#js-rate-filed').change(function(e) {
@@ -57,19 +62,19 @@ $(document).ready(function () {
         if (amount < 90) {
             $('.js-error').html('Минимальная сумма пополнения — $90<br>Пожалуйста, введите другую сумму');
             $('.js-display-two-submit').prop('disabled', true);
-            sessionStorage['payAmount'] = 0;
+            setCookie('payAmount', 0);
         } else if (amount > 1500) {
             $('.js-error').html('Максимальная сумма пополнения — $1500<br>Пожалуйста, введите другую сумму');
             $('.js-display-two-submit').prop('disabled', true);
-            sessionStorage['payAmount'] = 0;
+            setCookie('payAmount', 0);
         } else {
             $('.js-error').html('');
             $('.js-display-two-submit').prop('disabled', false);
             
-            sessionStorage['payAmount'] = amount;
+            setCookie('payAmount', amount);
         }
         
-        $('.js-pay-amount').text(sessionStorage['payAmount']);
+        $('.js-pay-amount').text(getCookie('payAmount'));
     }
     
     $('.js-display-two-submit').click(function (e) { 
@@ -86,7 +91,7 @@ $(document).ready(function () {
     });
     
     $('#go-to-three-display').click(function (e) {
-        $bankImage.attr('src', sessionStorage['bankImage']);
+        $bankImage.attr('src', getCookie('bankImage'));
         if ($bankImage.attr('src')) {
             $bankImage.show();
         } 
@@ -98,15 +103,16 @@ $(document).ready(function () {
     $('#image-file').change(function(e) {
         file = $('#image-file')[0].files[0];
         if (file){
+            console.log(file);
+            
             $('.js-display-three-submit').prop('disabled', false);
+            setCookie('receiptUrl', file);
         }  
     });
     
     $('.js-display-three-submit').click(function(e) {
         timerDecrement2();
     });
-    
-    
     
     
     const time       = $('#js-time');
@@ -129,6 +135,7 @@ $(document).ready(function () {
             }
             
             time.text(newTime);
+            setCookie('timer1', newTime);
             
             $seconds.text(seconds);
             $minutes.text(minutes);
@@ -181,4 +188,98 @@ $(document).ready(function () {
             $(this).html('<i class="bi bi-x-lg"></i> Скрыть');
         }
     });
+    
+    function getCookie(name) {
+        let matches = document.cookie.match(new RegExp(
+          "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+    
+    function setCookie(name, value, options = {}) {
+
+        options = {
+            path: '/',
+            ...options
+        };
+      
+        if (options.expires instanceof Date) {
+            options.expires = options.expires.toUTCString();
+        }
+      
+        let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+      
+        for (let optionKey in options) {
+            updatedCookie += "; " + optionKey;
+            let optionValue = options[optionKey];
+            if (optionValue !== true) {
+                updatedCookie += "=" + optionValue;
+            }
+        }
+      
+        document.cookie = updatedCookie;
+    }
+    
+    function deleteCookie(name) {
+        setCookie(name, "", {
+          'max-age': -1
+        })
+    }
+    
+    function deleteAllCookies() {
+        deleteCookie('bankImage');
+        deleteCookie('bankbankNameImage');
+        deleteCookie('payAmount');
+        deleteCookie('timer1');
+        deleteCookie('timer2');
+        deleteCookie('receiptUrl');
+        deleteCookie('step');
+    }
+    
+    // Next functions need for cookie. You should delete All cookies after end work "pay widget"
+    if (getCookie('bankImage')) {
+        $bankImage.attr('src', getCookie('bankImage'));
+        $bankImage.show();
+        $('#js-display-two-bank-name').text('"'+ getCookie('bankName') + '".');
+    }
+    
+    if (getCookie('bankName')) {
+        $bankImage.attr('src', getCookie('bankImage'));
+        $bankImage.show();
+    }
+    
+    if (getCookie('payAmount')) {
+        $('.js-pay-amount').text(getCookie('payAmount'));
+        $('.js-display-two-submit').text('Изменить сумму');
+        $('.js-diplay-two-second-block').show('fast');
+        $('#js-rate-filed').val(getCookie('payAmount'));
+        $('.js-display-two-submit').prop('disabled', false);
+    }
+    
+    if (getCookie('timer1')) {
+        time.text(getCookie('timer1'));
+        if(getCookie('timer1') > 0) timerDecrement();
+    }
+    
+    if (getCookie('timer2')) {
+        time2.text(getCookie('timer2'));
+        if(getCookie('timer2') > 0) timerDecrement2();
+    }
+    
+    if (getCookie('receiptUrl')) {
+        time2.text(getCookie('timer2'));
+        file = $('#image-file')[0].files[0];
+        
+        if (file){
+            $('.js-display-three-submit').prop('disabled', false);
+            setCookie('receiptUrl', file);
+            $('#image-file').val(file);
+        }  
+    }
+    
+    if (getCookie('step') && getCookie('step') != 1) {
+        var myModal = new bootstrap.Modal(document.getElementById('js-step-' + getCookie('step')));
+          
+        myModal.show();
+    }
 });
